ce0546774a522fc88cb93385e06a15f2fa285b59
Mehwish Aziz <mehwisha@msn.com>
2018-04-16 14:41:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,17 @@
  def turn_count(board)
    counter = 0
-  #  while counter <= board.length
-  #  if board == "X" || board == "O"
      board.each do |space|
           if space == "X" || space == "O"
           counter += 1
-
- # end
- # end
  end
  end
  return counter
  end
 
-
-
  def current_player(board)
-
-
+if turn_count % 2 == 0
+  puts "Player X is playing"
+elsif turn_count % 2 == 1
+  puts "Players O turn"
+end
 end
