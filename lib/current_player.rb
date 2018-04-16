867b200ce92cfde5833b4401c385b1e49b1558c4
Mehwish Aziz <mehwisha@msn.com>
2018-04-16 13:10:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,12 @@
  def turn_count(board)
    counter = 0
   while counter <= board.length-1
-    # if board == "X" || board == "Y"
+   if board == "X" || board == "Y"
        board.each do |space|
          if space == "X" || space == "Y"
          counter += 1
          return counter
+end
 end
 end
 end
