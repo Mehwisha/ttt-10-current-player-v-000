518491b08dddf75fc1773b7c36836216c808364f
Mehwish Aziz <mehwisha@msn.com>
2018-04-16 15:13:56 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@
  end
 
  def current_player(board)
-if turn_count(board) = 0 && turn_count(board) % 2 == 0
+if turn_count(board) == 0 && turn_count(board) % 2 == 0
   current_player(board) == "X"
 elsif turn_count(board) % 2 == 1
   current_player(board) == "O"
