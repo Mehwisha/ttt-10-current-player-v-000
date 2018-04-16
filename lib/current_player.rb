5f062ee2962885fdf0b258d9791fa1ab491804d6
Mehwish Aziz <mehwisha@msn.com>
2018-04-16 12:54:52 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,12 @@
  def turn_count(board)
    counter = 0
   while counter <= board.length
+    if board == "X" && board == "Y"
 board.each do |turns|
   counter += 1
 end
 return counter
+end
 end
 end
 
